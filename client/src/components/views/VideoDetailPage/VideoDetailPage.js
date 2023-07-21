7ab7f0f345af1f821d20d24d48981484592343c8
Narcoker <jykim9868@gmail.com>
2023-07-22 02:09:12 +0900

    ✨ Feat: 상세 페이지 구독 기능 구현 완료

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Row, Col, List, Avatar } from "antd";
 import Axios from "axios";
 import SideVideo from "./Sections/SideVideo";
+import Subscribe from "./Sections/Subscribe";
 
 function VideoDetailPage(props) {
   const videoId = props.match.params.videoId;
@@ -30,7 +31,14 @@ function VideoDetailPage(props) {
               src={`http://localhost:9999/${VideoDetail.filePath}`}
               controls
             />
-            <List.Item>
+            <List.Item
+              actions={[
+                <Subscribe
+                  userTo={VideoDetail.writer._id}
+                  userFrom={localStorage.getItem("userId")}
+                />,
+              ]}
+            >
               <List.Item.Meta
                 avatar={<Avatar src={VideoDetail.writer.image} />}
                 title={VideoDetail.writer.name}
